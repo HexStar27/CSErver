f3da2000c15373ce51a50a70f61ba4f2aefc903c
HexStar <theblaceman@gmail.com>
2022-04-23 00:36:00 +0200

    POR FIN
    respuesta de alegría en el root y ahora las peticiones se hacen con el header

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 //Zona principal donde se asignan rutas con funciones del servidor.
 
 var express = require("express");
-//const http  = require("http");
-//const https = require("https");
-//const fs    = require("fs");
-//const path  = require("path");
 
 var pScore  = require('./score');
 var auth    = require('./authentication');
@@ -19,19 +15,11 @@ app.use(express.urlencoded( {extended:false} ));
 app.use(express.json());
 
 
-//Obtencion del certificado SSL para habilitar el uso de HTTPS
-//const privateKey = fs.readFileSync( path.join(process.env.PATHCRT, process.env.KEY), 'utf-8');
-//const certificate = fs.readFileSync( path.join(process.env.PATHCRT,process.env.CRT), 'utf-8');
-//const credentials = {
-//    key: privateKey,
-//    cert: certificate
-//}; //Código zombi :(
-
 //Log in
-//body: [username]
+//head: [username]
 //      [password]
 app.post('/login', async (req,res)=>{
-    const{username,password} = req.body;
+    const{username,password} = req.head;
     let accessToken = await auth.Login(username, password);
     if(accessToken.length > 0)
     res.header('authorization',accessToken).json({
@@ -48,11 +36,11 @@ app.post('/login', async (req,res)=>{
 
 //---------------SCORE---------------//
 //Get Top10 score globally OR the scores of a specific difficulty.
-//body: [dif]  int
-//body: [tipo] int
+//head: [dif]  int
+//      [tipo] int
 app.get("/score", async (req,res)=>{
-    let dif = req.body["dif"];
-    let tipo = req.body["tipo"];
+    let dif = req.head["dif"];
+    let tipo = req.head["tipo"];
     if(tipo == null) tipo = 0;
     if(isNaN(dif)) res.json(await pScore.Top10());
     else res.json(await pScore.Score(dif,tipo));
@@ -60,31 +48,31 @@ app.get("/score", async (req,res)=>{
 
 //Save a score to the DB
 //head: [authorization]
-//body: [dif]  number
+//      [dif]  number
 //      [punt] number
 //      [user] string
 //      [used] number
 //      [time] number (float)
 app.post("/score", auth.validateToken, async (req,res)=>{
-    let dif = req.body["dif"];
-    let punt = req.body["punt"];
-    let used = req.body["used"];
-    let time = req.body["time"];
-    let id = await util.UsernameToID(req.body["user"],true);
+    let dif = req.head["dif"];
+    let punt = req.head["punt"];
+    let used = req.head["used"];
+    let time = req.head["time"];
+    let id = await util.UsernameToID(req.head["user"],true);
     res.json(await pScore.SaveScore(id,punt,dif,used,time));
 });
 
 //Return sum of scores of the given user
 //head: [authorization]
-//body: [user] string
+//      [user] string
 app.get("/score/total", auth.validateToken, async (req,res)=>{
-    let id = await util.UsernameToID(req.body["user"],true);
+    let id = await util.UsernameToID(req.head["user"],true);
     res.json(await pScore.CompleteScore(id));
 });
 
 //Calculates the score of a given data
 //head: [authorization]
-//body: [caso_id]   number
+//      [caso_id]   number
 //      [consultas] number
 //      [tiempo]    number
 //      [examen]    boolean
@@ -92,13 +80,13 @@ app.get("/score/total", auth.validateToken, async (req,res)=>{
 //      [dificultad] number
 //      [consulta]  string
 app.get("/score/calculate", auth.validateToken, async (req,res)=>{
-    let casoID = req.body["caso_id"];
-    let consultasUsadas = req.body["consultas"];
-    let tiempoEmpleado = req.body["tiempo"];
-    let casoExamen = req.body["examen"];
-    let retoOpcional = req.body["reto"];
-    let dificultad = req.body["dificultad"];
-    let consultaEvaluada = req.body["consulta"];
+    let casoID = req.head["caso_id"];
+    let consultasUsadas = req.head["consultas"];
+    let tiempoEmpleado = req.head["tiempo"];
+    let casoExamen = req.head["examen"];
+    let retoOpcional = req.head["reto"];
+    let dificultad = req.head["dificultad"];
+    let consultaEvaluada = req.head["consulta"];
 
     let p = await pScore.CalcularScore(casoID,consultaEvaluada, consultasUsadas,
                                 tiempoEmpleado,casoExamen, retoOpcional, dificultad);
@@ -108,9 +96,9 @@ app.get("/score/calculate", auth.validateToken, async (req,res)=>{
 
 //---------------EVENT---------------//
 //Returns a random event or an especific one
-//body: [id] number
+//head: [id] number
 app.get("/event", async (req,res)=>{
-    let id = req.body["id"];
+    let id = req.head["id"];
     if(isNaN(id)) res.json(await event.getRandomEvent());
     else res.json(await event.getEvent(id));
 });
@@ -119,19 +107,19 @@ app.get("/event", async (req,res)=>{
 //---------------GAME----------------//
 //Save the savefile of a user in the DB
 //head: [authorization]
-//body: [user] string
+//      [user] string
 //      [save] json
 app.post("/save", auth.validateToken, async (req,res)=>{
-    let id = await util.UsernameToID(req.body["user"],true);
-    let saveFile = req.body["save"];
+    let id = await util.UsernameToID(req.head["user"],true);
+    let saveFile = req.head["save"];
     res.json(await game.Save(id,saveFile));
 });
 
 //Load the savefile to a user of the DB
 //head: [authorization]
-//body: [user] string
+//      [user] string
 app.get('/load', auth.validateToken, async (req,res)=>{
-    let id = await util.UsernameToID(req.body["user"],true);
+    let id = await util.UsernameToID(req.head["user"],true);
     res.json(await game.Load(id));
 });
 
@@ -139,19 +127,19 @@ app.get('/load', auth.validateToken, async (req,res)=>{
 //---------------CASOS---------------//
 //Return N cases of a given difficulty
 //head: [authorization]
-//body: [dif]   number
+//      [dif]   number
 //      [casos] number
 app.get('/case', auth.validateToken, async (req,res)=>{
-    let dif = req.body["dif"];
-    let nCasos = req.body["casos"];
+    let dif = req.head["dif"];
+    let nCasos = req.head["casos"];
     res.json(await casos.GetCasosAlmacenados(dif,nCasos));
 });
 
 //Return one exam case of a given difficulty
 //head: [authorization]
-//body: [dif]   number
+//      [dif]   number
 app.get('/case/exam', auth.validateToken, async (req,res)=>{
-    let dif = req.body["dif"];
+    let dif = req.head["dif"];
     res.json(await casos.GetCasoExamen(dif));
 });
 
@@ -163,23 +151,20 @@ app.get('/case/final', auth.validateToken, async (req,res)=>{
 
 //Returns whether a proposed solution is valid or not
 //head: [authorization]
-//body: [caseid] number
+//      [caseid] number
 //      [caso]   number
 app.get('/case/solve', auth.validateToken, async (req,res)=>{
-    let caseID = req.body["caseid"];
-    let consulta = req.body["caso"];
+    let caseID = req.head["caseid"];
+    let consulta = req.head["caso"];
     if(util.AntiInjectionStringField(consulta,'app',true))
         res.json(await casos.ResolverCaso(caseID, consulta));
     else res.json({info:"Incorrecto", res:"La consulta es sospechosa"});
 });
 
 
-
-//const httpServer = http.createServer(app);
-//const httpsServer = https.createServer(credentials, app);
-
-//httpServer.listen(process.env.PUERTO);
-//httpsServer.listen(process.env.SPUERTO);
+app.get('/', (req,res)=>{
+    res.json({respuesta:"POR FIN"});
+});
 
 app.listen(5000,()=>{
     console.log("Servicio operativo! " + debug.logFullDate());
