52f4868d0afd01e54b299cfc0157766dcbdfa25d
HexStar <theblaceman@gmail.com>
2022-04-25 21:56:55 +0200

    loglogloglog

--- authentication.js
+++ authentication.js
@@ -41,6 +41,7 @@ async function Login(username, password)
 {
     //Consulta BD para ver si existe usuario
     let v = await util.ValidLogin(username,password,true);
+    console.log(v);
     if(!v) return "";
     //Información a encriptar para generar el token
     const info = {username : username, password : password};
