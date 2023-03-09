ddf3ce96b2f93e096852e55d5ee65633fd3dee53
HexStar <62072214+HexStar27@users.noreply.github.com>
2023-03-09 21:56:20 +0100

    Hecho

--- case.js
+++ case.js
@@ -186,7 +186,6 @@ async function ResolverCaso(casoID, qPropuesta)
 async function RealizarConsulta(consulta){
 
     if(consulta.indexOf(';') >= 0 || !consulta.includes('SELECT')) {
-        logError("Error, "+consulta+", "+!consulta.includes('SELECT'),'case');
         return {info:"Incorrecto",res:"La consulta no debe tener ; y debe haber al menos un SELECT"}
     }
     comoqueno = ["INSERT","DELETE","UPDATE", "CREATE", "USE", 
