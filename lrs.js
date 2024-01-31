682070ba6c3447c3005f9b2da53a823b4aa537f1
HexStar <62072214+HexStar27@users.noreply.github.com>
2024-01-31 10:51:42 +0100

    huh

--- lrs.js
+++ lrs.js
@@ -24,6 +24,7 @@ async function AddStatement(json)
 {
     if (json == undefined) return -1;
     console.log("Buen parse: "+json);
+    json = JSON.parse(json);
     console.log(typeof(json));
     if (check.SearchForKeyWords(json,"lsr"))
     {
