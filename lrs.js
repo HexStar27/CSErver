22e67ae905ec77f69af048d7605288d20f55517f
HexStar <62072214+HexStar27@users.noreply.github.com>
2024-02-23 14:10:41 +0100

    Uuuuh

--- lrs.js
+++ lrs.js
@@ -24,44 +24,41 @@ async function AddStatement(rawjson)
 {
     if (rawjson == undefined) return -1;
     let json = JSON.parse(rawjson);
-    if (check.SearchForKeyWords(rawjson,"lsr"))
-    {
-        let actor = JSON.stringify(json["actor"]["mbox"]);
-        if(actor == undefined) return -1;
-        else actor = "'"+actor+"'";
+    
+    let actor = JSON.stringify(json["actor"]["mbox"]);
+    if(actor == undefined) return -1;
+    else actor = "'"+actor+"'";
 
-        let verb = JSON.stringify(json["verb"]["id"]);
-        if(verb == undefined) return -1;
-        else verb = "'"+verb+"'";
+    let verb = JSON.stringify(json["verb"]["id"]);
+    if(verb == undefined) return -1;
+    else verb = "'"+verb+"'";
 
-        let obj = JSON.stringify(json["object"]["id"]);
-        if(obj == undefined) return -1;
-        else obj = "'"+obj+"'";
+    let obj = JSON.stringify(json["object"]["id"]);
+    if(obj == undefined) return -1;
+    else obj = "'"+obj+"'";
 
-        let res = JSON.stringify(json["result"]);
-        if(res == undefined) res = "null";
-        else res = "'"+res+"'";
+    let res = JSON.stringify(json["result"]);
+    if(res == undefined) res = "null";
+    else res = "'"+res+"'";
 
-        let ctx = JSON.stringify(json["context"]);
-        if(ctx == undefined) ctx = "null";
-        else ctx = "'"+ctx+"'";
+    let ctx = JSON.stringify(json["context"]);
+    if(ctx == undefined) ctx = "null";
+    else ctx = "'"+ctx+"'";
 
-        let time = json["timestamp"];
-        if(time == undefined) time = "null";
-        else time = "'"+time+"'";
+    let time = json["timestamp"];
+    if(time == undefined) time = "null";
+    else time = "'"+time+"'";
 
-        let consulta = "INSERT INTO learningRecord (actor, verb, object, result, context, timestamp, whole_statement) VALUES ("+
-        actor+", "+verb+", "+obj+", "+res+", "+ctx+", "+time+", '"+rawjson+"')";
-        try {
-            await db.query(consulta);
-            return 1;
-        }
-        catch(err) {
-            logError("Error de consulta en al intentar almacenar el registro: "+err,'lsr');
-            return -1;
-        }
+    let consulta = "INSERT INTO learningRecord (actor, verb, object, result, context, timestamp, whole_statement) VALUES ("+
+    actor+", "+verb+", "+obj+", "+res+", "+ctx+", "+time+", '"+rawjson+"')";
+    try {
+        await db.query(consulta);
+        return 1;
     }
-    else return -1;
+    catch(err) {
+        logError("Error de consulta en al intentar almacenar el registro: "+err,'lsr');
+        return -1;
+    }
 }
 
 module.exports.AddListOfStatements = AddListOfStatements;
